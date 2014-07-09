8d9cca8db72f325e7d7c9e487a46dedb5d52d10e
vikash-vinsol <vikashsoni@Vikashs-MacBook-Air.local>
2014-07-09 12:54:56 +0530

    File3 add

---
+++ GItTagExercise/File3.h
@@ -0,0 +1,13 @@
+//
+//  File3.h
+//  GItTagExercise
+//
+//  Created by Vikash Soni on 09/07/14.
+//  Copyright (c) 2014 Vikash Soni. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface File3 : NSObject
+
+@end
---
+++ GItTagExercise/File3.m
@@ -0,0 +1,13 @@
+//
+//  File3.m
+//  GItTagExercise
+//
+//  Created by Vikash Soni on 09/07/14.
+//  Copyright (c) 2014 Vikash Soni. All rights reserved.
+//
+
+#import "File3.h"
+
+@implementation File3
+
+@end
