0e068aff943ab28e9d263a38ddc4a92532ad5cb3
vikash-vinsol <vikashsoni@Vikashs-MacBook-Air.local>
2014-07-09 12:57:12 +0530

    Remove waarning method

--- GItTagExercise/ViewController.m
+++ GItTagExercise/ViewController.m
@@ -20,9 +20,5 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 @end
